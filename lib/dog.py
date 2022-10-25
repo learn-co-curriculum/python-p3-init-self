6963924dada3cf7dca84cc909dc0fed94d29e659
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-25 10:52:45 -0500

    add solution

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
